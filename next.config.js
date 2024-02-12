4f6609cc3acf1b8cad88a5a46052a7a1b8aa2e59
Santiya Georgieva <santiya.georgigeva@gmail.com>
2024-02-12 13:38:48 +0200

    fix project config for build

--- next.config.js
+++ next.config.js
@@ -2,12 +2,19 @@
 const path = require('path')
 
 const nextConfig = {
+    output: 'export',
     basePath: '',
     reactStrictMode: true,
     swcMinify: true,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    images: {
+        loader: 'custom',
+        loaderFile: './my-loader.ts',
+        formats:['image/webp'],
+        unoptimized: true
+    }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
